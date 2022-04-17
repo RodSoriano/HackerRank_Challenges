c9bd81d27ffcf53854c74a4dc9c89f3c2cd936de
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-04-16 20:53:03 -0600

    select exercise we used order by

---
+++ SQL-Basic/EmployeeSalary.sql
@@ -0,0 +1,8 @@
+--MySQL
+
+SELECT name
+FROM Employee
+WHERE Salary > 2000 AND months < 10
+ORDER BY employee_id ASC;
+
+--https://www.hackerrank.com/challenges/salary-of-employees/problem?isFullScreen=true
---
+++ SQL-Basic/employee Names.sql
@@ -0,0 +1,7 @@
+--MySQL
+
+SELECT name
+FROM Employee
+ORDER BY name ASC;
+
+--https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
