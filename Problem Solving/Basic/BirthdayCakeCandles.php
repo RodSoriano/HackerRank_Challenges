04bc8426d9b46d8cfb49415709a402086a4d7567
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-04-22 13:50:19 -0600

    algorithm basic problem

---
+++ Problem Solving/Basic/BirthdayCakeCandles.php
@@ -0,0 +1,23 @@
+<?php
+
+# only blow the tallest candle
+#candles = [4,4,1,3]
+
+function birthdayCakeCandles($candles)
+    {
+        $result = 0;
+        $max = max($candles);
+       
+        for($i = 0; $i < count($candles); $i++)
+            {
+                if($candles[$i] == $max)
+                    {
+                        $result ++;
+                    }
+            }
+        return $result;
+    }
+
+#https://www.hackerrank.com/challenges/birthday-cake-candles/problem?isFullScreen=false
+
+?>
