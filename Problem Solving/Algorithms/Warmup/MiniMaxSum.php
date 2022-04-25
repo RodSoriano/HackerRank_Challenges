196ac38539fb4fdf9327cd14cf3091f8e642e3f3
Rodrigo Soriano <95240720+RodSoriano@users.noreply.github.com>
2022-04-25 17:37:58 -0600

    Create MiniMaxSum.php
    still in progress

---
+++ Problem Solving/Algorithms/Warmup/MiniMaxSum.php
@@ -0,0 +1,30 @@
+<?php
+
+# giving a 5 position array, find
+# max sum using 4 elements of the array
+# min sum using 4 elements of the array
+
+function miniMaxSum($arr)
+    {
+        $min = min($arr);
+        $max = max($arr);
+        $end = count($arr);
+        $result;
+        
+        for($i = 0; $i < $end; $i++)
+            {
+                if($arr[$i] > $min)
+                    {
+                        
+                    }
+            }
+        
+    }
+
+#testing 
+$arr = [1,2,3,4,5];
+#min = 6
+#max = 14
+
+# miniMaxSum($arr);
+?>
