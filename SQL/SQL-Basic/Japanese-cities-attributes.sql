115ef796c9f1139f0830b2d7db8450245a8ff397
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-06-25 02:41:32 -0600

    japanese select basic excercise

---
+++ SQL/SQL-Basic/Japanese-cities-attributes.sql
@@ -0,0 +1,5 @@
+--MySQL--
+
+SELECT * FROM CITY WHERE COUNTRYCODE = "JPN";
+
+--https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?isFullScreen=true--
---
+++ SQL/SQL-Basic/Japanese-cities-names.sql
@@ -0,0 +1,5 @@
+--MySQL--
+
+SELECT NAME FROM CITY WHERE COUNTRYCODE = "JPN";
+
+--https://www.hackerrank.com/challenges/japanese-cities-name/problem?isFullScreen=true--
