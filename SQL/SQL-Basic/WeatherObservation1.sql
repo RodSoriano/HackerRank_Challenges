651db357990a24af9045dcb6659483e62ab7ab91
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-06-25 00:59:04 -0600

    file rename

--- SQL/SQL-Basic/WeatherObservation1.sql
+++
@@ -1,6 +0,0 @@
---MySQL
-
-SELECT CITY, STATE
-FROM STATION;
-
---https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true
--- SQL/SQL-Basic/WeatherObservation2.sql
+++
@@ -1,6 +0,0 @@
---MySQL
-
-SELECT CONCAT(ROUND(SUM(LAT_N),2),SPACE(1),ROUND(SUM(LONG_W),2))
-FROM STATION;
-
---https://www.hackerrank.com/challenges/weather-observation-station-2/problem?isFullScreen=true
