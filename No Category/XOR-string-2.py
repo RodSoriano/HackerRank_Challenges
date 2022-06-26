f819eb49360078efae078539b06ab6b0cf076873
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-06-26 16:51:29 -0600

    python basic excercise

---
+++ No Category/XOR-string-2.py
@@ -0,0 +1,36 @@
+#n this challenge, the task is to debug the existing code to successfully execute all provided test files.
+#Debug the given function strings_xor to find the XOR of the two given strings appropriately. 
+#Note: You can modify at most three lines in the given code and you cannot add or remove lines to the code.
+
+# Original code
+def strings_xor1(s, t): #here I added a "1" so it would not give error on de IDE for naming a function with the same name twice
+    res = "";
+    for i in range(len(s)):
+        if s[i] = t[i]:
+            res = '0';
+        else:
+            res = '1';
+
+    return res
+
+s = input()
+t = input()
+print(strings_xor1(s, t))
+
+# Debug code
+
+def strings_xor(s, t):
+    res = ""
+    for i in range(len(s)):
+        if s[i] == t[i]: # added another equal to compare if they are the same
+            res = res + '0'; # concatenated the previous answer with the added number so res can save previous data run in the loop
+        else:
+            res = res + '1'; # same thing as line 26
+
+    return res
+
+s = input()
+t = input()
+print(strings_xor(s, t))
+
+#https://www.hackerrank.com/challenges/one-month-preparation-kit-strings-xor/problem?h_r=internal-search
