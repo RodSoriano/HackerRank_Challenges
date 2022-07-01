8564d75c0c37b3807b762f33adaa94c6bda2ae78
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-06-30 21:38:03 -0600

    math basic problem with js

---
+++ JavaScript/Mathematics/Fundamentals/maximum-draws.js
@@ -0,0 +1,8 @@
+//If there are n colors of socks in the drawer,
+//how many socks need to be removed to be certain of having a matching pair?
+
+function maximumDraws(n) {
+    return n + 1;
+}
+
+//https://www.hackerrank.com/challenges/maximum-draws/problem?isFullScreen=true
