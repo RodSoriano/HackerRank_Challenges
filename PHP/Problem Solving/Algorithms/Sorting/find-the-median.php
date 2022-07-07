b708acd81a0e8a312c420a9cf2d70e632ec38796
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-07-07 10:14:02 -0600

    sorting basic excercise

--- PHP/Problem Solving/Algorithms/Sorting/find-the-median.php
+++ PHP/Problem Solving/Algorithms/Sorting/find-the-median.php
@@ -2,33 +2,9 @@
 
 #The median of a list of numbers is essentially its middle element after sorting.
 
-#excecution time error, too slow 
-#seems like bubble sorting is to slow for hacker ranks tests, 
-#still working on it
-
 function findMedian($arr) {
     $length = count($arr);
-    $swapped = false;
-    $temp = 0;
-    $result = 0;
-
-    do
-        {
-            $swapped = false;
-            
-            for($i = 0;$i < $length - 1; $i++)
-                {
-                    if($arr[$i] > $arr[$i + 1])
-                        {
-                            $temp = $arr[$i];
-                            $arr[$i] = $arr[$i + 1];
-                            $arr[$i + 1] = $temp;
-                            $swapped = true;
-                        }   
-                }
-        }
-    while($swapped);
-
+    sort($arr);
     $result = floor($length/2);
 
     return $arr[$result];
